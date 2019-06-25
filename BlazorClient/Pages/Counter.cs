904c6ba4caff6bc76761dc9d877680aac05fad7a
Simon Pinn <simon@spinn.net.au>
2019-06-25 17:00:15 +1000

    Getting to know blazor

---
+++ BlazorClient/Pages/Counter.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Components;
+using BlazorClient.Services;
+
+namespace BlazorClient.Pages
+{
+    public class Counter_ : ComponentBase
+    {
+        private IAppStateService stateService;
+
+        [Inject]
+        protected IAppStateService StateService
+        {
+            get
+            {
+                return stateService;
+            }
+            set
+            {
+                stateService = value;
+                if (stateService != null)
+                {
+                    stateService.OnStateChanged += () => StateHasChanged(); 
+                }
+            }
+        }
+
+        protected int currentCount => stateService.Counter;        
+
+        protected override void OnAfterRender()
+        {
+            var timer = new System.Timers.Timer(1000);
+            timer.Elapsed += Timer_Elapsed;
+            timer.Start();
+
+            base.OnAfterRender();
+        }
+
+        private void Timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            
+        }
+
+        protected void IncrementCount()
+        {
+            stateService.IncrementCounter();
+        }
+    }
+}
---
+++ BlazorClient/Services/AppStateService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorClient.Services
+{
+    public interface IAppStateService
+    {
+        event AppStateServiceEvent OnStateChanged;
+        int Counter { get; }
+        void IncrementCounter();
+    }
+
+    public delegate void AppStateServiceEvent();
+
+    public class AppStateService : IAppStateService
+    {
+        public event AppStateServiceEvent OnStateChanged;
+
+        public AppStateService()
+        {
+            var timer = new System.Timers.Timer(1000);
+            timer.Elapsed += Timer_Elapsed;
+            timer.Start();
+        }
+
+        private void Timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            IncrementCounter();
+            OnStateChanged();
+        }
+
+        public int Counter { get; private set; }
+        public void IncrementCounter()
+        {
+            Counter++;
+        }
+    }
+}
